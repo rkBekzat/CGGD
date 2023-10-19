bb1b777b91afb5168e501cc115b101a14c520916
Bekzat <rakhimbayevbekzat@gmail.com>
2023-10-19 15:12:21 +0300

    fix CI

--- src/utils/resource_utils.cpp
+++ src/utils/resource_utils.cpp
@@ -7,32 +7,31 @@
 
 #include <stb_image_write.h>
 
-
-using namespace cg::utils;
-
 std::string view_command(const std::filesystem::path& path)
 {
 #ifdef __APPLE__
-	return std::string("open ").append(path.string());
+    return std::string("open ").append(path.string());
 #endif
-#ifdef __WINDOWS__
-	return std::string("start ").append(path.string());
+#ifdef _WIN32
+    return std::string("start ").append(path.string());
 #endif
-	return path.string();
+    return "";
 }
 
-void save_resource(cg::resource<cg::unsigned_color>& render_target, const std::filesystem::path& filepath)
+void cg::utils::save_resource(cg::resource<cg::unsigned_color>& render_target, const std::filesystem::path filepath)
 {
-	int width = static_cast<int>(render_target.get_stride());
-	int height = static_cast<int>(render_target.get_number_of_elements()) / width;
+    int width = static_cast<int>(render_target.get_stride());
+    int height = static_cast<int>(render_target.get_number_of_elements()) / width;
 
-	int result = stbi_write_png(
-			filepath.string().c_str(), width, height, 3, render_target.get_data(),
-			width * sizeof(cg::unsigned_color));
+    int result = stbi_write_png(
+            filepath.string().c_str(), width, height, 3, render_target.get_data(),
+            width * sizeof(cg::unsigned_color));
 
-	if (result != 1)
-		THROW_ERROR("Can't save the resource");
+    if (result != 1)
+        THROW_ERROR("Can't save the resource");
 
-	std::system(view_command(filepath).c_str());
+    auto command = view_command(filepath);
+    if (!command.empty())
+        std::system(command.c_str());
 }
 
